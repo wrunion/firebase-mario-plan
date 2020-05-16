5aca72965f9c5fd284e6ae77d932a7966e550ee5
wrunion <winterrunion@gmail.com>
2020-05-15 22:23:36 -0700

    Add SignedInLinks.

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
 
 const NavBar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
-        <Link to="/" className="brand-logo">MarioPlan</Link>
+        <Link to="/" className="brand-logo left">MarioPlan</Link>
+        <SignedInLinks />
       </div>
     </nav>
   );
