f085b9ac5ce0490ae529e439524e8ffba0d98a2b
wrunion <winterrunion@gmail.com>
2020-05-15 22:18:10 -0700

    Add NavBar component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import NavBar from './layouts/NavBar';
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>MarioPlan</h1>
+        <NavBar />
       </div>
     </BrowserRouter>
   );
