fe314277e4679a506a331a74d049f431ff9605e1
wrunion <winterrunion@gmail.com>
2020-05-15 21:29:00 -0700

    Add redux, thunk, react-redux, and react-router-dom.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>MarioPlan</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>MarioPlan</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
 
 //Tutorial here: https://www.youtube.com/watch?v=r5b0spRlnlU&list=PL94kpoa-L28IYDb7Boa4p5KyFuVmvEMfO&index=3
