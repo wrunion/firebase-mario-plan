51db297eacf2114285667eb75682b1c66114f23c
wrunion <winterrunion@gmail.com>
2020-05-15 21:18:38 -0700

    Inital commit

--- src/App.js
+++ src/App.js
@@ -5,22 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MarioPlan</h1>
     </div>
   );
 }
 
 export default App;
+
+
+//Tutorial here: https://www.youtube.com/watch?v=r5b0spRlnlU&list=PL94kpoa-L28IYDb7Boa4p5KyFuVmvEMfO&index=3
