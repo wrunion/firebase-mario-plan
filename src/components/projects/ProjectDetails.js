30d81af7110f8b7fbb9fca308fa30653e8002ff2
wrunion <winterrunion@gmail.com>
2020-05-17 10:24:35 -0700

    Create SignIn component.

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export default function ProjectDetails() {
+export default function ProjectDetails(props) {
+  const id = props.match.params.id;
+
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title</span>
+          <span className="card-title">Project Title - {id}</span>
           <p>Lorem ipsum dollar set asdlfjalsdfklasdfkjalsdfkaldfj</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
