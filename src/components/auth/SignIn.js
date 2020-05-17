402a654cdf9551a4d5ee473762913430f7b5ebf2
wrunion <winterrunion@gmail.com>
2020-05-17 10:34:01 -0700

    Complete SignIn component.

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 class SignIn extends Component {
   state ={
-
+    email: '',
+    password: ''
   }
   handleChange = (e) => {
-    console.log(e);
+    this.setState({
+      [e.target.id]: e.target.value
+    })
   }
 
   handleSubmit = (e) => {
-    console.log(e);
+    e.preventDefault();
+    console.log(this.state);
   }
 
   render() {
