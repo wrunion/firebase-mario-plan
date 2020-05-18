d305476a2caf60ff4f9dff19761d323cb67e4a6c
wrunion <winterrunion@gmail.com>
2020-05-18 15:23:40 -0700

    Add firebase and firestore; some unresolved config errors.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk';
+import { getFirestore, reduxFirestore } from 'redux-firestore';
+import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
+/* GET THIS INFO BACK FROM .env AND PUT IT IN FBCONFIG.JS */
+import fbConfig from './config/fbConfig';
 
-const store=createStore(rootReducer, applyMiddleware(thunk));
+const store=createStore(rootReducer, 
+  compose(  
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,16 @@
 export const createProject = project => {
-  return (dispatch, getState) => {
-    //make async call to database
-    dispatch({type: 'CREATE_PROJECT', project});
+  return (dispatch, getState, {getFirebase, getFirestore}) => {
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project, 
+      authorFirstName: 'Net',
+      authorLastName: 'Ninja',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({type: 'CREATE_PROJECT', project});
+    }).catch((err) =>  [
+      dispatch({type: 'CREATE_PROJECT_ERROR', err})
+    ])
   }
 }
