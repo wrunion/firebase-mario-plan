9648303aad2910d991ebd53efc615bb9c93b78f9
wrunion <winterrunion@gmail.com>
2020-05-17 21:34:37 -0700

    All project components now connected to Redux.

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export default function ProjectSummary() {
+export default function ProjectSummary({project}) {
   return (
     <div>
       <div className="card z-depth-0 project-summary">
-      <div className="card-content grey-text darkened-3">
-        <span className="card-title">Project Title</span>
-        <p>Posted by Shaun</p>
-        <p className="grey-text">September 26th, 5pm</p>
-    </div>
-    </div> 
+        <div className="card-content grey-text darkened-3">
+          <span className="card-title">{project.title}</span>
+          <p>Posted by Shaun</p>
+          <p className="grey-text">September 26th, 5pm</p>
+        </div>
+      </div> 
     </div>
   )
 }
