442597fee1143db71bdc1a85ab006be38772f13f
wrunion <winterrunion@gmail.com>
2020-05-17 21:27:00 -0700

    Create store and reducers.

--- src/components/layouts/SignedInLinks.js
+++ src/components/layouts/SignedInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const SignedInLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to='/'>New Project</NavLink> </li>
+      <li><NavLink to='/createproject'>New Project</NavLink> </li>
       <li><NavLink to='/'>Log Out</NavLink> </li>
       <li><NavLink to='/' className="btn btn-floating pink lighten-1">NJ</NavLink> </li>
     </ul>
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+  projects: [
+    {id: '1', title: 'Help me find Peach!', content: 'Mario ipsum RGB mushroom 1-up. Pokey stopwatch heart? Heart life invincible Princess Toadstool potion snift pokey tweeter ostro Nintendo Entertainment System? Heart starman Luigi spark Mushroom Kingdom spark coin? Pow autobomb A-button invincible Toad tweeter. '},
+    {id: '2', title: 'Collect all the stars', content: 'astle beezo radio frequency modulator, birdo game pak shell game pak Mario game pak. Invincible Princess Toadstool heart shell platform start button mushroom Japan potion NES select button 8-bit starman Princess Toadstool, stopwatch ostro game pak! Platform shell 8-bit run A-button, Luigi ninji RGB 1-up rocket invincible Princess Toadstool Nintendo Entertainment System cobrat 1-up, Luigi jump.' },
+    {id: '3', title: 'Egg hunt with Yoshi', content: 'oopa paratroopa fire flower? Jump koopa paratroopa plumber reset swim red shell pipe super mushroom, side-scrolling Mario? Blooper podoboo koopa paratroopa time? B-button PAL platform podoboo slide lava fire bar pixel starman brick invincible Mario 8-bit. Super Mario lakitu warp zone, pipe castle NES bill blaster. NES radio frequency modulator fire bar NES Mario cloud koopa troopa.'}
+  ]
+};
+
+const projectReducer = (state = initState, action) => {
+  return state;
+}
+
+export default projectReducer;
