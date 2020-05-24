e073c687fe6279a075564c1ea3aa4cb0d39d0f3e
wrunion <winterrunion@gmail.com>
2020-05-24 08:48:50 -0700

    Add fbConfig and use env variables to set auth info.

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,1 +1,20 @@
-/* NOTE: GET ALL THIS STUFF FROM .env BEFORE WORKING ON THIS AGAIN */
+import firebase from 'firebase/app';
+/* OR: import * as firebase from 'firebase'; */
+import 'firebase/firestore';
+import 'firebase/auth';
+
+/* Initialize Firebase */
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducers/rootReducer';
-import thunk from 'redux-thunk';
-import { getFirestore, reduxFirestore } from 'redux-firestore';
-import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
-/* GET THIS INFO BACK FROM .env AND PUT IT IN FBCONFIG.JS */
-import fbConfig from './config/fbConfig';
+// import thunk from 'redux-thunk';
+// import { getFirestore, reduxFirestore } from 'redux-firestore';
+// import { getFirebase, reactReduxFirebase } from 'react-redux-firebase';
+// /* GET THIS INFO BACK FROM .env AND PUT IT IN FBCONFIG.JS */
+// import fbConfig from './config/fbConfig';
 
-const store=createStore(rootReducer, 
-  compose(  
-    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
-  )
-);
+// const store=createStore(rootReducer, 
+//   compose(  
+//     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+//     reduxFirestore(fbConfig),
+//     reactReduxFirebase(fbConfig)
+//   )
+// );
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={createStore(rootReducer)}>
     <App />
   </Provider>,
   document.getElementById('root')
